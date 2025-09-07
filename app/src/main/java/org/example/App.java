b1992b20cb183f004dcc098acd3e3a521c9c4045
Karly Latkiewicz <ksunny1242@gmail.com>
2025-09-07 17:11:35 -0500

    Finished Oops shall we try again

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,11 +1,25 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
-  public int getInput() {
+  public static int getInput(String p, String error, int lower, int upper) {
     // Implement your function here
+    Scanner input = new Scanner(System.in);
+    int i = 0;
+    System.out.println(p);
+    i = input.nextInt();
+    while(i < lower || i > upper){
+      System.out.println(error);
+      System.out.println(p);
+      i = input.nextInt();
+    }
+    return i;
   }
 
   public static void main(String[] args) {
     // Test your function here in a driver program
+    System.out.println(getInput("Please enter a value", "Your message is invalid", 1, 9));
+    
   }
 }
